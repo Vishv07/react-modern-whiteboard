2a4b82f061e8520a71a9f29168c0652c36d9130a
Vishv Kakadiya <krishnakakadiya9@gmail.com>
2021-05-03 17:39:45 +0530

    Temp Css Settlement

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import WhiteBoard from './Components/whiteBoard';
 import Tools from './Components/tools';
 import History from './Components/history';
-
+import "./index.css";
 ReactDOM.render(<div id='main'>
 					<div id='container'>
 						<Tools/>
